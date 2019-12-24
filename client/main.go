fdec4b1593f46567d170094c37c12c6e55e864f0
Gustavo Caso <gustavocaso@gmail.com>
2019-12-23 19:27:08 -0500

    create client struct

--- client/main.go
+++ client/main.go
@@ -15,6 +15,75 @@ import (
 	"nhooyr.io/websocket"
 )
 
+type client struct {
+	userName string
+	conn     *websocket.Conn
+	ctx      context.Context
+	message  chan string
+}
+
+func (c *client) close() {
+	defer c.conn.Close(websocket.StatusInternalError, "the sky is falling")
+}
+
+func (c *client) listen() {
+	for {
+		_, reader, err := c.conn.Reader(c.ctx)
+		if err != nil {
+			log.WithError(err).Warn("Error receiving message")
+			break
+		} else {
+			io.Copy(os.Stdout, reader)
+			fmt.Println("")
+		}
+	}
+}
+
+func (c *client) getInput() {
+	for {
+		in := bufio.NewReader(os.Stdin)
+		result, err := in.ReadString('\n')
+		if err != nil {
+			log.WithError(err).Fatal(err)
+		}
+
+		c.message <- result
+	}
+}
+
+func (c *client) run() {
+	go c.listen()
+	go c.getInput()
+Loop:
+	for {
+		select {
+		case text := <-c.message:
+			err := c.conn.Write(c.ctx, websocket.MessageText, []byte(text))
+			if err != nil {
+				log.WithError(err).Fatal("Error sending message")
+				break
+			}
+		case <-c.ctx.Done():
+			log.Fatal("Client session ended")
+		case <-time.After(4000 * time.Millisecond):
+			continue Loop
+		}
+	}
+}
+
+func newClient(ctx context.Context, userName, addr string) *client {
+	c, _, err := websocket.Dial(ctx, addr, nil)
+	if err != nil {
+		log.WithError(err).Fatal("Error opening a connection to server")
+	}
+	return &client{
+		userName: userName,
+		conn:     c,
+		ctx:      ctx,
+		message:  make(chan string, 1),
+	}
+}
+
 var (
 	port     uint
 	chatRoom string
@@ -45,54 +114,7 @@ func main() {
 	}()
 
 	url := fmt.Sprintf("ws://localhost:%d/chat/%s/%s", port, user, chatRoom)
-	c, _, err := websocket.Dial(ctx, url, nil)
-	if err != nil {
-		log.WithError(err).Fatal("Error opening a connection to server")
-	}
-	log.Info("Connected")
-	defer c.Close(websocket.StatusInternalError, "the sky is falling")
+	client := newClient(ctx, user, url)
 
-	go func() {
-		for {
-			_, reader, err := c.Reader(ctx)
-			if err != nil {
-				log.WithError(err).Warn("Error receiving message")
-				break
-			} else {
-				io.Copy(os.Stdout, reader)
-				fmt.Println("")
-			}
-		}
-	}()
-
-	input := make(chan string, 1)
-	go getInput(input)
-
-Loop:
-	for {
-		select {
-		case text := <-input:
-			err = c.Write(ctx, websocket.MessageText, []byte(text))
-			if err != nil {
-				log.WithError(err).Fatal("Error sending message")
-				break
-			}
-		case <-ctx.Done():
-			log.Fatal("Client session ended")
-		case <-time.After(4000 * time.Millisecond):
-			continue Loop
-		}
-	}
-}
-
-func getInput(input chan string) {
-	for {
-		in := bufio.NewReader(os.Stdin)
-		result, err := in.ReadString('\n')
-		if err != nil {
-			log.WithError(err).Fatal(err)
-		}
-
-		input <- result
-	}
+	client.run()
 }
